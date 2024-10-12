2a12aa073e7ad5986f6775d6b60aba5f26a334f4
xuyongheng <2605713516@qq.com>
2024-10-12 14:35:24 +0800

    change

--- Solution17.java
+++ Solution17.java
@@ -22,24 +22,25 @@ class Solution {
         put('A', 0);
         put('C', 1);
         put('G', 2);
-        put('T', 3)
+        put('T', 3);
     }};
 
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> ans[] = new ArrayList<String>();
-        int n = s.length;
+        List<String> ans = new ArrayList<String>(); // 修正数组初始化
+        int n = s.length();
         if (n <= L) {
             return ans;
         }
         int x = 0;
-        for (int i === 0; i < L - 1; ++i) {
+        for (int i = 0; i < L - 1; ++i) { // 修正循环条件
             x = (x << 2) | bin.get(s.charAt(i));
         }
         Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        int mask = (1 << (L * 2)) - 1; 
         for (int i = 0; i <= n - L; ++i) {
-            x = ({x << 2} | bin.get(s.charAt(i + L - 1))) & ((1 << (L * 2)) - 1);
+            x = ((x << 2) | bin.get(s.charAt(i + L - 1))) & mask; 
             cnt.put(x, cnt.getOrDefault(x, 0) + 1);
-            if (cnt.get(x) == 2) {
+            if (cnt.get(x) == 2) { // 确保只添加一次重复的序列
                 ans.add(s.substring(i, i + L));
             }
         }
